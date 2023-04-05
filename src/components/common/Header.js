35ae8c5b33c9469b83b50b92ddc98e4150ff81b4
xongjaemin <woals99@gmail.com>
2023-04-06 00:27:25 +0900

    add: 로그인 레이아웃

--- src/components/common/Header.js
+++ src/components/common/Header.js
@@ -1,17 +1,20 @@
 import React from "react";
 import styled from "@emotion/styled";
+import { Link } from "react-router-dom";
 
 function Header() {
   return (
     <Container>
       <Logo>Formvey</Logo>
-      <Login>로그인</Login>
+      <Link to="/login">
+        <Login>로그인</Login>
+      </Link>
     </Container>
   );
 }
 
 const Container = styled.div`
-  width: 94vw;
+  width: 100vw;
   height: 60px;
   background: rgba(163, 188, 255, 0.3);
   padding-left: 3vw;
@@ -20,6 +23,8 @@ const Container = styled.div`
   align-items: center;
   justify-content: space-between;
   position: fixed;
+
+  backdrop-filter: blur(10px);
 `;
 
 const Logo = styled.div``;
@@ -28,6 +33,7 @@ const Login = styled.div`
   font-weight: 400;
   font-size: 20px;
   cursor: pointer;
+  color: black;
 `;
 
 export default Header;
---
+++ src/components/login/LoginTextInput.js
@@ -0,0 +1,26 @@
+import React from "react";
+import styled from "@emotion/styled";
+
+function LoginTextInput(props) {
+  return (
+    <>
+      <Input placeholder={props.placeholder} />
+    </>
+  );
+}
+
+const Input = styled.input`
+  width: 336px;
+  height: 48px;
+  background: #f7f7f7;
+  border: 0.3px solid #000000;
+  border-radius: 5px;
+  font-weight: 400;
+  font-size: 15px;
+  padding-left: 19px;
+  padding-right: 19px;
+
+  margin-top: 23px;
+`;
+
+export default LoginTextInput;
