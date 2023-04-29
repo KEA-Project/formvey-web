c4be9b043f7c810b466c1b9dd0863d741e5b7878
xongjaemin <woals99@gmail.com>
2023-04-29 14:03:07 +0900

    dev: 카카오 로그인

---
+++ src/components/login/KakaoRedirectHandler.js
@@ -0,0 +1,45 @@
+import React, { useEffect } from "react";
+import qs from "qs";
+import axios from "axios";
+
+function KakaoRedirectHandler() {
+  let code = new URL(window.location.href).searchParams.get("code"); //인가코드
+
+  //폼베이 서버 카카오 로그인 처리
+  const kakaoLogin = async (accessToken) => {
+    //console.log(accessToken);
+    const response = await axios.post(
+      `${process.env.REACT_APP_BASE_URL}/login/kakao/${accessToken}`
+    );
+
+    console.log(response.data);
+    console.log(response.data.isSuccess);
+  };
+
+  //엑세스 토큰 발급 받기
+  const getToken = async () => {
+    //console.log(code);
+    const payload = qs.stringify({
+      grant_type: "authorization_code",
+      client_id: process.env.REACT_APP_KAKAO_KEY,
+      redirect_uri: process.env.REACT_APP_KAKAO_REDIRECT,
+      code: code,
+    });
+
+    const response = await axios.post(
+      "https://kauth.kakao.com/oauth/token",
+      payload
+    );
+
+    console.log(response.data.access_token);
+    kakaoLogin(response.data.access_token);
+  };
+
+  useEffect(() => {
+    getToken();
+  }, []);
+
+  return null;
+}
+
+export default KakaoRedirectHandler;
