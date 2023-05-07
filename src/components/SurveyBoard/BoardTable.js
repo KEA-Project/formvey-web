e881d5af8d5d2286c10dd97a95f647922ba97a46
xongjaemin <woals99@gmail.com>
2023-05-07 21:44:33 +0900

    add: 설문게시판에서 설문 응답하기

--- src/components/SurveyBoard/BoardTable.js
+++ src/components/SurveyBoard/BoardTable.js
@@ -1,44 +1,59 @@
-import React from 'react';
+import React from "react";
 import styled from "@emotion/styled";
-import { Link } from "react-router-dom";
-
+import { Link, useNavigate } from "react-router-dom";
 
 function BoardTable(props) {
+  const navigate = useNavigate();
 
-    return (
-        <Container>
-        <Table>
+  const navigateParticipate = (memberId, surveyId) => {
+    if (memberId == localStorage.getItem("memberId")) {
+      //내가 만든 설문일 경우
+      console.log("내 설문");
+    } else {
+      //내가 만든 설문이 아닐 경우
+      navigate(`/participate/${surveyId}`);
+    }
+  };
+
+  return (
+    <Container>
+      <Table>
         <TableHead>
-            <TableRow>
-            <TableHeaderCell className='first'>설문 제목</TableHeaderCell>
+          <TableRow>
+            <TableHeaderCell className="first">설문 제목</TableHeaderCell>
             <TableHeaderCell>D-Day</TableHeaderCell>
             <TableHeaderCell>응답자 수</TableHeaderCell>
-            <TableHeaderCell className='last'>설문 제작자</TableHeaderCell>
-            </TableRow>
+            <TableHeaderCell className="last">설문 제작자</TableHeaderCell>
+          </TableRow>
         </TableHead>
         <TableBody>
-        {props.survey.map((survey) => {
-            if(survey.dday === 0) survey.dday = '완료';
-            else{
-                survey.dday = 'D - ' + survey.dday;
+          {props.survey.map((survey) => {
+            if (survey.dday === 0) survey.dday = "완료";
+            else {
+              survey.dday = "D - " + survey.dday;
             }
-            return(
-          <TableRow key={survey.id}>
+            return (
+              <TableRow
+                key={survey.surveyId}
+                onClick={() => {
+                  navigateParticipate(survey.memberId, survey.surveyId);
+                }}
+              >
                 <TableCell>{survey.surveyTitle}</TableCell>
                 <TableCell>{survey.dday}</TableCell>
                 <TableCell>{survey.responseCnt}</TableCell>
                 <TableCell>{survey.nickname}</TableCell>
-          </TableRow>);
-        })}
+              </TableRow>
+            );
+          })}
         </TableBody>
       </Table>
-      </Container>
-    );
+    </Container>
+  );
 }
 
 const Container = styled.div`
   width: 100%;
-
 `;
 
 const Table = styled.table`
@@ -53,23 +68,22 @@ const Table = styled.table`
   line-height: 1.2;
   text-align: center;
   margin: 0 auto;
-
 `;
 
 const TableHead = styled.thead`
-  background: #EBF2FF;
+  background: #ebf2ff;
 
   font-weight: 500;
   font-size: 15px;
 
   line-height: 20px;
 
-  .first{
+  .first {
     border-top-left-radius: 10px;
     width: 60%;
   }
 
-  .last{
+  .last {
     border-top-right-radius: 10px;
   }
 `;
@@ -79,10 +93,11 @@ const TableBody = styled.tbody`
 `;
 
 const TableRow = styled.tr`
-//   &:nth-of-type(even) {
-//     background-color: #f8f9fa;
-//   }
+  //   &:nth-of-type(even) {
+  //     background-color: #f8f9fa;
+  //   }
 
+  cursor: pointer;
 `;
 
 const TableHeaderCell = styled.th`
@@ -90,7 +105,6 @@ const TableHeaderCell = styled.th`
   vertical-align: top;
 
   border-bottom: 1px solid #dee2e6;
-  
 `;
 
 const TableCell = styled.td`
@@ -98,7 +112,5 @@ const TableCell = styled.td`
   vertical-align: top;
   border-bottom: 1px solid #dee2e6;
 `;
-
-
 
 export default BoardTable;
