a8c7a938b61f62e08de22621fede9c426b375cee
LeeYeonhee-00 <98442485+LeeYeonhee-00@users.noreply.github.com>
2023-05-08 23:40:44 +0900

    짧폼 응답 모달(get)

---
+++ src/components/shortForm/ShortFormModal.js
@@ -0,0 +1,239 @@
+import React, { useState, useEffect } from "react";
+import styled from "@emotion/styled";
+import goToSurvey from "../../assets/shortForm/goToSurvey.png";
+import nextShortVector from "../../assets/shortForm/nextShortVector.png";
+import shortTitleVector from "../../assets/shortForm/shortTitleVector.png";
+import axios from "axios";
+
+function ShortFormModal() {
+  const [listShort, setListShort] = useState([]);
+
+  // 짧폼 랜덤 조회
+  const fetchData = async () => {
+    const shortRandom = await axios.get(
+      `${
+        process.env.REACT_APP_BASE_URL
+      }/shortforms/random/${localStorage.getItem("memberId")}`,
+      {
+        headers: {
+          "X-ACCESS-TOKEN": localStorage.getItem("jwt"),
+        },
+      }
+    );
+    console.log(shortRandom);
+    if (shortRandom.data.isSuccess) {
+      setListShort(shortRandom.data.result);
+    }
+
+    // const sendResponse = async () => {
+    //   const response = await axios.post(
+    //     `${
+    //       process.env.REACT_APP_BASE_URL
+    //     }/shortanswers/${shortformId}/${localStorage.getItem("memberId")}`,
+    //     {
+    //       shortAnswer: answers,
+    //     },
+    //     {
+    //       headers: {
+    //         "X-ACCESS-TOKEN": localStorage.getItem("jwt"),
+    //       },
+    //     }
+    //   );
+    // };
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const shortFormUpdate = () => {
+    fetchData();
+    setSelected([]); // 다음 질문으로 넘어갈 때 선택 초기화
+    setSelectedMulti([]); // 다음 질문으로 넘어갈 때 선택 초기화
+  };
+
+  // 단일 선택
+  const [selected, setSelected] = useState(-1); // 선택된 버튼의 인덱스 값
+
+  const handleSelect = (index) => {
+    setSelected(index);
+  };
+
+  // 다중 선택
+  const [selectedMulti, setSelectedMulti] = useState([]); // 선택된 버튼의 인덱스 값
+
+  const handleMultiSelect = (index) => {
+    if (selectedMulti.includes(index)) {
+      // 이미 선택된 버튼일 경우
+      setSelectedMulti(selectedMulti.filter((item) => item !== index)); // 선택된 버튼에서 제거
+    } else {
+      setSelectedMulti([...selectedMulti, index]); // 선택된 버튼 배열에 추가
+    }
+  };
+
+  return (
+    <Container>
+      <Headers>
+        <ShortTitleVector src={shortTitleVector} />
+        <ShortTitle>{listShort.surveyTitle}</ShortTitle>
+        <GoToSurvey src={goToSurvey} />
+      </Headers>
+      <Body>
+        <ShortQuestion>{listShort.shortQuestion}</ShortQuestion>
+
+        {listShort.shortType === 0 ? ( //객관식 단일선택
+          <OptionDiv>
+            {listShort.options.map((option) => {
+              return (
+                <ShortOption
+                  key={option.shortIndex}
+                  selected={selected === option.shortIndex}
+                  onClick={() => handleSelect(option.shortIndex)}
+                >
+                  {option.shortContent}
+                </ShortOption>
+              );
+            })}
+          </OptionDiv>
+        ) : listShort.shortType === 1 ? ( //객관식 다중선택
+          <OptionDiv>
+            {listShort.options.map((option) => {
+              return (
+                <ShortOption
+                  key={option.shortIndex}
+                  selected={selectedMulti.includes(option.shortIndex)}
+                  onClick={() => handleMultiSelect(option.shortIndex)}
+                >
+                  {option.shortContent}
+                </ShortOption>
+              );
+            })}
+          </OptionDiv>
+        ) : (
+          //주관식
+          <>
+            <AnswerInput />
+          </>
+        )}
+      </Body>
+      <NextShortVector src={nextShortVector} onClick={shortFormUpdate} />
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  width: 500px;
+  height: 680px;
+  margin-top: 20px;
+  margin-left: 20px;
+
+  border: 0.01em solid #d2d2d2;
+  border-radius: 15px;
+  box-shadow: 0px 2px 30px rgba(0, 0, 0, 0.06);
+  padding: 20px 20px 15px 20px;
+
+  .flexDiv {
+    display: flex;
+    align-items: center;
+  }
+
+  position: relative;
+  overflow: visible;
+`;
+
+const Headers = styled.div`
+  margin-top: 10px;
+  margin-left: 20px;
+  display: flex;
+  flex-direction: row;
+  // justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+`;
+
+const ShortTitleVector = styled.img`
+  width: 10px;
+  height: 20px;
+`;
+
+const ShortTitle = styled.div`
+  font-size: 13px;
+  color: #101828;
+  font-weight: 500;
+  margin-left: 15px;
+`;
+
+const GoToSurvey = styled.img`
+  width: 120px;
+  height: 40px;
+  cursor: pointer;
+  margin-right: 15px;
+  margin-left: auto;
+`;
+
+const Body = styled.div`
+  margin-top: 10px;
+  margin-left: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 20px;
+  // height: auto;
+`;
+
+const ShortQuestion = styled.div`
+  font-size: 30px;
+  color: #101828;
+  font-weight: 700;
+  margin-top: 20%;
+  flex-align: left;
+  margin-bottom: 50px;
+`;
+
+const OptionDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const ShortOption = styled.button`
+  width: 400px;
+  height: 60px;
+  border-radius: 8px;
+  // background-color: #f9f9f9;
+  background-color: ${({ selected }) => (selected ? "#a7a7a7" : "#f9f9f9")};
+  border: 1px solid #ececec;
+  margin-top: 10px;
+  font-size: 20px;
+  color: #101828;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+const NextShortVector = styled.img`
+  width: 95px;
+  height: 15px;
+  //고정 값 말고 어떻게 설정하는지,,
+  margin-bottom: 30px;
+  margin-left: 200px;
+  cursor: pointer;
+  position: fixed;
+  bottom: 0;
+`;
+
+const AnswerInput = styled.input`
+  width: 400px;
+  height: 60px;
+  border-radius: 8px;
+  background-color: #f9f9f9;
+  border: 1px solid #ececec;
+  margin-top: 10px;
+  font-size: 20px;
+  color: #101828;
+  font-weight: 600;
+  cursor: pointer;
+`;
+
+export default ShortFormModal;
